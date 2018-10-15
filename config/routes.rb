9b5b0db45ba5d5a34af1c49b2ccbdb7f5aa5599a
Ming-Cee Yee <mingcee@skysketches.com>
2018-10-15 01:18:38 -0400

    display wantlist partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#index'
+  root to: 'users#index'
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
   end
 
   resources :users do
-
   end
 end
